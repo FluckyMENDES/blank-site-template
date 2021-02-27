ead693354feb09681de8c68eefc55027880db051
FluckyMENDES <flucky@mail.ru>
2021-02-27 14:01:14 +0300

    Инициализация проекта

---
+++ src/blocks/pages/index/main/main.js
@@ -0,0 +1,19 @@
+import Swiper from 'swiper/bundle';
+window.Swiper = Swiper;
+
+new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.next',
+    prevEl: '.prev',
+  },
+});
---
+++ src/js/import/libs.js
@@ -0,0 +1,2 @@
+import '%libs%/simple-modal/simple-modal';
+import '%libs%/focus-visible/focus-visible';
